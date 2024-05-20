a833aa60ffc37d8ad3ac95a523fc7a514cb383bb
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2024-05-20 13:28:46 +0100

    add  Dockcerfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+    port: 8081,
+    strictPort: true,
+  },
   server: {
-    hmr: {
-      host: 'library-bjeh.onrender.com',
-      protocol: 'wss', // Use WebSocket Secure since your site uses HTTPS
-    },
-    protocol: 'wws',
-    host: '0.0.0.0',
-    port:10000
+    port: 8081,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8081",
   },
 });
