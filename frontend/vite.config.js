c74d017b239548c3e22cdb4c6e1d3adf10109497
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2024-05-20 12:01:59 +0100

    conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
-      host: 'localhost',
+      host: 'library-bjeh.onrender.com',
+      protocol: 'wss', // Use WebSocket Secure since your site uses HTTPS
     },
-  }
-})
+    host: '0.0.0.0', // This allows connections from any IP address
+    port: 3000, // Ensure this port is open in Render's settings
+  },
+});
